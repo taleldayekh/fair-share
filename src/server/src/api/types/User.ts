588f84a92336f38d6a21c53f2f8e01180c9f0a42
taleldayekh <mail@talel.se>
2020-04-13 18:55:37 +0200

    Adding GraphQL user type

--- src/server/src/api/types/User.ts
+++ src/server/src/api/types/User.ts
@@ -1,7 +1,26 @@
+import { DBUser } from '../../interfaces/types';
+import getUserSpendingGroups from '../queries/getUserSpendingGroups';
+
 export const User = `
   type User {
     id: ID!
-    name: String!
-    spendingGroup: SpendingGroup
+    username: String!
+    spendingGroups: [SpendingGroup]
   }
 `;
+
+export class UserType {
+  public id: string;
+  public username: string;
+  public spendingGroupsId?: [string];
+
+  constructor(db_row: DBUser) {
+    this.id = db_row.id;
+    this.username = db_row.username;
+    this.spendingGroupsId = db_row.spendingGroupsId;
+  }
+
+  spendingGroups() {
+    return getUserSpendingGroups(this.spendingGroupsId);
+  }
+}
