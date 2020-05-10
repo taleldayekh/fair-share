8b8ffa2037f2531a04bfbfd2af137ca0ff90a177
taleldayekh <mail@talel.se>
2020-05-10 13:22:31 +0200

    Adding db connection

--- src/server/src/db/db.ts
+++ src/server/src/db/db.ts
@@ -1,1 +1,23 @@
 const r = require('rethinkdb');
+
+// TODO: Environment variables for connecting to either test db or prod db
+const DB_OPTIONS = {
+  db: 'fairsharedb',
+};
+// TODO: Environment variables for connect options and create separate connections for dev, test and prod
+const CONNECT_OPTIONS = {
+  host: 'localhost',
+  port: 28015,
+};
+const config = {
+  ...DB_OPTIONS,
+  ...CONNECT_OPTIONS,
+};
+
+let connection = null;
+
+// TODO: Replace any type
+r.connect(config, (err: any, conn: any) => {
+  if (err) throw err;
+  connection = conn;
+});
