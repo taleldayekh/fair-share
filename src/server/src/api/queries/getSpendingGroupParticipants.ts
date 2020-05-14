09b9d6c4c2a4779e2c065c3cb69c3467bebf1b01
taleldayekh <mail@talel.se>
2020-05-14 23:27:21 +0200

    Cleaning out outdated files

--- src/server/src/api/queries/getSpendingGroupParticipants.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@/api/types/User';
-import { db } from '@/api/tests/db';
-
-export default (participants: number[]): User[] => {
-  const spendingGroupParticipants = db.users
-    .filter((user) => participants.includes(user.id))
-    .map((user) => new User(user));
-  return spendingGroupParticipants;
-};
--- src/server/src/api/queries/getUserSpendingGroups.ts
+++
@@ -1,9 +0,0 @@
-import { SpendingGroup } from '@/api/types/SpendingGroup';
-import { db } from '@/api/tests/db';
-
-export default (spendingGroups: number[]): SpendingGroup[] => {
-  const userSpendingGroups = db.spendingGroups
-    .filter((spendingGroup) => spendingGroups.includes(spendingGroup.id))
-    .map((spendingGroup) => new SpendingGroup(spendingGroup));
-  return userSpendingGroups;
-};
