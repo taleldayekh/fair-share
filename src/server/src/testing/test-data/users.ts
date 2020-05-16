2fd5e9e76ee8eafd3623d2f7444085c8d6704c37
taleldayekh <mail@talel.se>
2020-05-16 10:23:51 +0200

    Initial code for testing db and test data

---
+++ src/server/src/testing/test-data/users.ts
@@ -0,0 +1,14 @@
+import { TALEL_ID, BIANCA_ID } from './constants';
+
+export const users = [
+  {
+    id: TALEL_ID,
+    name: 'Talel Dayekh',
+    email: 'mail@talel.se',
+  },
+  {
+    id: BIANCA_ID,
+    name: 'Bianca Basan',
+    email: 'mail@biancabasan.com',
+  },
+];
