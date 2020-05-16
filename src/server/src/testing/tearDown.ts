5626077e82b0edac54523ecb7f2b754a4be5b776
taleldayekh <mail@talel.se>
2020-05-16 10:41:04 +0200

    Test teardown

---
+++ src/server/src/testing/tearDown.ts
@@ -0,0 +1,7 @@
+import { r } from 'rethinkdb-ts';
+// import { testData } from './test-data';
+
+export default async () => {
+  await r.dbDrop('testdb').run();
+  (await r.connectPool()).drain();
+};
