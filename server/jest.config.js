ba5868667c9f9b452ff6035ad4691a36b634003e
taleldayekh <mail@talel.se>
2020-05-17 23:52:22 +0200

    Trying to close db connection in test tearDown

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
   globalSetup: '<rootDir>/src/testing/setUp.ts',
-  // globalTeardown: '<rootDir>/src/testing/tearDown.ts',
+  globalTeardown: '<rootDir>/src/testing/tearDown.ts',
 };
