59d5da332b4c5debcd2a8536124e36ed1fb550d2
taleldayekh <mail@talel.se>
2020-05-17 22:20:37 +0200

    Hiding global tearDown for CI

--- server/src/testing/tearDown.ts
+++ server/src/testing/tearDown.ts
@@ -1,7 +1,7 @@
-import { r } from 'rethinkdb-ts';
-// import { testData } from './test-data';
+// import { r } from 'rethinkdb-ts';
+// // import { testData } from './test-data';
 
-export default async () => {
-  await r.dbDrop('testdb').run();
-  (await r.connectPool()).drain();
-};
+// export default async () => {
+//   await r.dbDrop('testdb').run();
+//   (await r.connectPool()).drain();
+// };
