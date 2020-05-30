d88ce1445d8f60e60b8223e75295a51fc0b0d350
taleldayekh <mail@talel.se>
2020-05-30 10:34:28 +0200

    Interfaces for spending group/spending group participant

--- server/src/interfaces/types.ts
+++ server/src/interfaces/types.ts
@@ -3,3 +3,18 @@ export interface IDBUser {
   name: string;
   email: string;
 }
+
+export interface IDBSpendingGroup {
+  id: string;
+  name: string;
+  ownerId: string;
+  participants: ISpendingGroupParticipant[];
+  totalGroupSpending: boolean;
+}
+
+export interface ISpendingGroupParticipant {
+  id: string;
+  name: string;
+  email: string;
+  totalParticipantSpending: boolean;
+}
