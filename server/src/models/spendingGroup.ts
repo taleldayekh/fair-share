0089b2270124d29dfe751cc3a26a579c9ec1d0a5
taleldayekh <mail@talel.se>
2020-06-01 19:22:35 +0200

    Db query for creating new spending group

---
+++ server/src/models/spendingGroup.ts
@@ -0,0 +1,24 @@
+import { db } from '../db';
+import { IDBSpendingGroup } from '../interfaces/types';
+
+type NewSpendingGroup = {
+  userId: string;
+  name: string;
+};
+
+type CreateSpendingGroupArgs = NewSpendingGroup;
+
+const createSpendingGroup = async (
+  userId: string,
+  name: string,
+): Promise<IDBSpendingGroup> => {
+  return await db
+    .table('spending_groups')
+    .insert({ owner_id: userId, name: name })
+    .do((res) => {
+      return db.table('spending_groups').get(res('generated_keys').nth(0));
+    })
+    .run();
+};
+
+export { CreateSpendingGroupArgs, createSpendingGroup };
