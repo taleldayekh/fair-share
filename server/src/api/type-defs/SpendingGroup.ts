71a9f04fa35621f4b624b948d85203918241be55
taleldayekh <mail@talel.se>
2020-06-05 17:44:20 +0200

    SpendingGroup and Spending type defs

--- server/src/api/type-defs/SpendingGroup.ts
+++ server/src/api/type-defs/SpendingGroup.ts
@@ -1,7 +1,17 @@
-// const SpendingGroupType = /* GraphQL */ `
-//   type SpendingGroup {
-//     id: ID!
-//     ownerId: String!
-//     name: String!
-//   }
-// `;
+export const SpendingGroupType = /* GraphQL */ `
+  type SpendingGroup {
+    id: ID!
+    ownerId: String!
+    name: String!
+    spending: Spending
+  }
+`;
+
+export const SpendingType = /* GraphQL */ `
+  type SpendingType {
+    id: ID!
+    ownerId: String!
+    spendingGroupId: String!
+    amount: Float
+  }
+`;
