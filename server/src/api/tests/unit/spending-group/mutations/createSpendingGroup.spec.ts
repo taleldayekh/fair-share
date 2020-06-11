6a19f1d249b541dcaf295c14cfe1a1e721c69d48
taleldayekh <mail@talel.se>
2020-06-11 19:04:40 +0200

    Tests for createSpendingGroup resolver

--- server/src/api/tests/unit/spending-group/mutations/createSpendingGroup.spec.ts
+++ server/src/api/tests/unit/spending-group/mutations/createSpendingGroup.spec.ts
@@ -1,3 +1,25 @@
 import * as spendingGroupModels from '../../../../../models/spendingGroup';
 import { testData } from '../../../../../shared/testing/test-data';
 import createSpendingGroup from '../../../../mutations/spending-group/createSpendingGroup';
+
+describe('createSpendingGroup resolver', () => {
+  const mockedCreateSpendingGroupModel = jest.spyOn(
+    spendingGroupModels,
+    'createSpendingGroup',
+  );
+
+  afterAll(() => {
+    mockedCreateSpendingGroupModel.mockRestore();
+  });
+
+  test('createSpendingGroup returns a new spending group', async () => {
+    const groceriesSpendingGroup = testData.spendingGroups[0];
+    mockedCreateSpendingGroupModel.mockResolvedValue(groceriesSpendingGroup);
+    const newSpendingGroup = await createSpendingGroup(
+      testData.users[0].id,
+      'Groceries',
+    );
+
+    expect(newSpendingGroup).toMatchObject(groceriesSpendingGroup);
+  });
+});
