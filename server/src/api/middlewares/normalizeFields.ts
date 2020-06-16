9c35a66b53d8842575ea29750ccad33c46a7cc90
taleldayekh <mail@talel.se>
2020-06-16 17:55:30 +0200

    Initial test for changing object keys

--- server/src/api/middlewares/normalizeFields.ts
+++ server/src/api/middlewares/normalizeFields.ts
@@ -1,1 +1,31 @@
-const dbSpendingGroup = { id: '1', owner_id: '2', name: 'Groceries' };
+/*
+ * Helper function for converting database
+ * field names to resolver field names.
+ */
+
+// Todo: Replace any type
+export default (dbField: any) => {};
+
+const dbSpendingGroup = {
+  id: '1',
+  owner_id: '2',
+  name: 'Groceries',
+  spendings_field: ['bla', 'bla', 'bla'],
+};
+
+let test = Object.entries(dbSpendingGroup)
+  .map((entry) => {
+    if (entry[0] === 'spendings_field') {
+      entry[0] = 'spendingsField';
+    }
+    return entry;
+  })
+  .reduce((newObj, key) => {
+    // @ts-ignore
+    return {
+      ...newObj,
+      [key[0]]: key[1],
+    };
+  }, {});
+
+console.log(test);
