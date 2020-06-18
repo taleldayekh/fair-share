2ff6c9d8d698e66183a841363b419644990d03d8
taleldayekh <mail@talel.se>
2020-06-18 18:10:16 +0200

    Field name regex

--- server/src/api/middlewares/normalizeFields.ts
+++ server/src/api/middlewares/normalizeFields.ts
@@ -1,34 +1,23 @@
 /*
  * Helper function for converting database
  * field names to resolver field names.
- */
+*/
 
-// Todo: Replace any type
-// Todo: Look up normalizing fields in nested objects
-export default (dbField: any) => {
-  return Object.entries(dbField);
-};
+// Todo: Proper typing
+const fieldsTest = (dbRows: {}) => {
+  return Object.entries(dbRows).map(dbRow => {
+    if (dbRow[0].includes('_')) {
+      const regEx = /(_)((?<=_).{1})/
+    }
+  })
+}
 
-// const dbSpendingGroup = {
-//   id: '1',
-//   owner_id: '2',
-//   name: 'Groceries',
-//   spendings_field: ['bla', 'bla', 'bla'],
-// };
+const testFields = {
+  id: '1',
+  owner_id: '2',
+  name: 'Groceries',
+  spending_field: ['10', '100', '1000']
+}
 
-// let test = Object.entries(dbSpendingGroup)
-//   .map((entry) => {
-//     if (entry[0] === 'spendings_field') {
-//       entry[0] = 'spendingsField';
-//     }
-//     return entry;
-//   })
-//   .reduce((newObj, key) => {
-//     // @ts-ignore
-//     return {
-//       ...newObj,
-//       [key[0]]: key[1],
-//     };
-//   }, {});
-
-// console.log(test);
+const normalizedFields = fieldsTest(testFields)
+console.log(normalizedFields)
