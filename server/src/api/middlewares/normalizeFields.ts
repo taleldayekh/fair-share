c8f6d6c50e0fda806e1c3dd5a79b56fcf4dddde8
taleldayekh <mail@talel.se>
2020-06-19 13:15:41 +0200

    Linting

--- server/src/api/middlewares/normalizeFields.ts
+++ server/src/api/middlewares/normalizeFields.ts
@@ -1,23 +1,23 @@
 /*
  * Helper function for converting database
  * field names to resolver field names.
-*/
+ */
 
 // Todo: Proper typing
 const fieldsTest = (dbRows: {}) => {
-  return Object.entries(dbRows).map(dbRow => {
+  return Object.entries(dbRows).map((dbRow) => {
     if (dbRow[0].includes('_')) {
-      const regEx = /(_)((?<=_).{1})/
+      const regEx = /(_)((?<=_).{1})/;
     }
-  })
-}
+  });
+};
 
 const testFields = {
   id: '1',
   owner_id: '2',
   name: 'Groceries',
-  spending_field: ['10', '100', '1000']
-}
+  spending_field: ['10', '100', '1000'],
+};
 
-const normalizedFields = fieldsTest(testFields)
-console.log(normalizedFields)
+const normalizedFields = fieldsTest(testFields);
+console.log(normalizedFields);
