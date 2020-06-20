ca659dd4a36ebff5899f436677362d00600c6d9d
taleldayekh <mail@talel.se>
2020-06-20 13:32:43 +0200

    Attempt to normalize fields

--- server/src/api/middlewares/normalizeFields.ts
+++ server/src/api/middlewares/normalizeFields.ts
@@ -5,17 +5,33 @@
 
 // Todo: Proper typing
 const fieldsTest = (dbRows: {}) => {
-  return Object.entries(dbRows).map((dbRow) => {
-    if (dbRow[0].includes('_')) {
-      const regEx = /(_)((?<=_).{1})/;
-    }
-  });
+  return Object.entries(dbRows)
+    .map((dbRow) => {
+      if (dbRow[0].includes('_')) {
+        const regEx = /(?<=_).{1}/g;
+        const snakeCase = dbRow[0].match(regEx);
+
+        if (!snakeCase) return;
+
+        dbRow[0].replace('_', '');
+        dbRow[0].replace(regEx, () => snakeCase.shift()!.toUpperCase());
+      }
+      console.log(dbRow);
+      return dbRow;
+    })
+    .reduce((dbRows, dbRow) => {
+      // Todo: Check for undefined
+      return {
+        ...dbRows,
+        [dbRow![0]]: dbRow![1],
+      };
+    }, {});
 };
 
 const testFields = {
   id: '1',
   owner_id: '2',
-  name: 'Groceries',
+  name_ttalel_dayekh: 'Groceries',
   spending_field: ['10', '100', '1000'],
 };
 
