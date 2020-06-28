ea95ba84bf581ff6c62ecd03ff900da1a87fb140
taleldayekh <mail@talel.se>
2020-06-28 12:08:43 +0200

    Code clean up

--- server/src/api/utils/normalizeFields.ts
+++ server/src/api/utils/normalizeFields.ts
@@ -1,75 +1,43 @@
-// TODO: Remove import
-const util = require('util');
-
 /*
  * Helper function for converting database
  * field names to resolver field names.
  */
 
-const normalizeFields = (dbRows: {}): {} => {
-  const dbRowsNormalized = Object.entries(dbRows)
-    .map((dbRow) => {
-      if (dbRow[0].includes('_')) {
+export const normalizeFields = (dbFields: {}): {} => {
+  // if (dbFields instanceof Array) return dbFields;
+  // console.log(dbFields instanceof Array);
+  const normalizedDBFields = Object.entries(dbFields)
+    .map((dbField) => {
+      if (dbField[0].includes('_')) {
         const snakeCaseLettersRegEx = /(?<=_).{1}/g;
-        const snakeCaseLetters = dbRow[0].match(snakeCaseLettersRegEx);
-        const resolverFieldName = dbRow[0]
+        const snakeCaseLetters = dbField[0].match(snakeCaseLettersRegEx);
+        const resolverField = dbField[0]
           .replace(snakeCaseLettersRegEx, () =>
             snakeCaseLetters!.shift()!.toUpperCase(),
           )
           .replace(/_/g, '');
 
-        dbRow[0] = resolverFieldName;
+        dbField[0] = resolverField;
       }
 
-      if (dbRow[1] instanceof Array) {
-        // !!
-        const bla = dbRow[1].map((row) => {
-          if (typeof row === 'object') {
-            return normalizeFields(row);
-          } else {
-            return row;
-          }
+      if (dbField[1] instanceof Array) {
+        const normalizedDBFields = dbField[1].map((dbField) => {
+          if (typeof dbField !== 'object' || dbField instanceof Array)
+            return dbField;
+          return normalizeFields(dbField);
         });
 
-        console.log(bla);
-        dbRow[1] = bla;
-        // !!
+        dbField[1] = normalizedDBFields;
       }
-      // !!
-      // returns array with key in one end and value in the other
-      // check if the key has an underscore
-      // Assign the key to a new variable, remove underscore and add uppercase letters
-      // Assign the new variable back to dbrow
-      return dbRow;
+
+      return dbField;
     })
-    .reduce((dbRowsNormalized, dbRows) => {
+    .reduce((normalizedDBFields, dbFields) => {
       return {
-        ...dbRowsNormalized,
-        [dbRows[0]]: dbRows[1],
+        ...normalizedDBFields,
+        [dbFields[0]]: dbFields[1],
       };
     }, {});
 
-  return dbRowsNormalized;
+  return normalizedDBFields;
 };
-
-const testDBRows = {
-  id: '1',
-  owner_id: '2',
-  name: 'Groceries',
-  spending_amount_field: [
-    'First random string',
-    { id: 1, total_spending: 100 },
-    { id: 2, total_spending: 200 },
-    {
-      id: 3,
-      spending_amount: [
-        'Second random string',
-        { id: 1, total_spending: 300 },
-        { id: 2, total_spending: 400 },
-      ],
-    },
-  ],
-};
-
-const normalizeTestDBRows = normalizeFields(testDBRows);
-console.log(util.inspect(normalizeTestDBRows, false, null, true));
