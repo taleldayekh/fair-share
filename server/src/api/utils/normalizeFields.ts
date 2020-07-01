c80d13dde129d29c3cd6e0cc26303de191109811
taleldayekh <mail@talel.se>
2020-07-01 19:07:56 +0200

    Initial attempt at normalizing fields in nested arrays

--- server/src/api/utils/normalizeFields.ts
+++ server/src/api/utils/normalizeFields.ts
@@ -3,41 +3,71 @@
  * field names to resolver field names.
  */
 
-export const normalizeFields = (dbFields: {}): {} => {
-  // if (dbFields instanceof Array) return dbFields;
-  // console.log(dbFields instanceof Array);
-  const normalizedDBFields = Object.entries(dbFields)
-    .map((dbField) => {
-      if (dbField[0].includes('_')) {
-        const snakeCaseLettersRegEx = /(?<=_).{1}/g;
-        const snakeCaseLetters = dbField[0].match(snakeCaseLettersRegEx);
-        const resolverField = dbField[0]
-          .replace(snakeCaseLettersRegEx, () =>
-            snakeCaseLetters!.shift()!.toUpperCase(),
-          )
-          .replace(/_/g, '');
+export const normalizeFields = () => {}
 
-        dbField[0] = resolverField;
-      }
 
-      if (dbField[1] instanceof Array) {
-        const normalizedDBFields = dbField[1].map((dbField) => {
-          if (typeof dbField !== 'object' || dbField instanceof Array)
-            return dbField;
-          return normalizeFields(dbField);
-        });
 
-        dbField[1] = normalizedDBFields;
-      }
 
-      return dbField;
-    })
-    .reduce((normalizedDBFields, dbFields) => {
-      return {
-        ...normalizedDBFields,
-        [dbFields[0]]: dbFields[1],
-      };
-    }, {});
 
-  return normalizedDBFields;
-};
+
+
+
+// /*
+//  * Helper function for converting database
+//  * field names to resolver field names.
+//  */
+
+// export const normalizeFields = (dbTable: {}): {} => {
+//   const normalizedFields = Object.entries(dbTable)
+//     .map((dbRow) => {
+//       if (dbRow[0].includes('_')) {
+//         const snakeCaseLettersRegEx = /(?<=_).{1}/g;
+//         const snakeCaseLetters = dbRow[0].match(snakeCaseLettersRegEx);
+//         const resolverFieldNames = dbRow[0]
+//           .replace(snakeCaseLettersRegEx, () =>
+//             snakeCaseLetters!.shift()!.toUpperCase(),
+//           )
+//           .replace(/_/g, '');
+
+//         dbRow[0] = resolverFieldNames;
+//       }
+
+
+
+//       if (dbRow[1] instanceof Array) {
+//         const normalizedFields = dbRow[1].map((field) => {
+//           if (typeof field !== 'object') {
+//             // console.log(field);
+//             return field;
+//           }
+
+//           // !!
+//           if (field instanceof Array) {
+//             const normalizedFields = field.map((bla) => {
+//               if (typeof bla !== 'object') {
+//                 return bla
+//               }
+
+//               return normalizeFields(bla)
+//             })
+//             return normalizedFields
+//           }
+//           // !!
+
+//           return normalizeFields(field);
+//         });
+//         console.log(normalizedFields)
+//         dbRow[1] = normalizedFields;
+//       }
+
+//       return dbRow;
+//     })
+//     .reduce((normalizedDBFields, dbFields) => {
+//       return {
+//         ...normalizedDBFields,
+//         [dbFields[0]]: dbFields[1],
+//       };
+//     }, {});
+
+//   return normalizedFields;
+// };
