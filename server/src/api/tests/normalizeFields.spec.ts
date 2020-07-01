d8921d2143cc30306fd87fa4bdbb630aae6e5cbb
taleldayekh <mail@talel.se>
2020-07-01 22:05:11 +0200

    Finalizing algorithm and tests for normalizeFields

--- server/src/api/tests/normalizeFields.spec.ts
+++ server/src/api/tests/normalizeFields.spec.ts
@@ -1,5 +1,34 @@
 import { normalizeFields } from '../utils/normalizeFields';
 
 describe('normalizeFields', () => {
-  test('smoke test', () => {});
+  const mixedFields = {
+    owner_id: '1',
+    name: 'Spending Group',
+    spending: [
+      'Mixed Spending',
+      { spending_group_id: '2', amount: [] },
+      {
+        spending_group_id: '2',
+        amount: [{ spending_one: 100, spending_two: 200 }],
+      },
+    ],
+    spenders: [[{ spender_name: 'Talel Dayekh' }], []],
+  };
+
+  test('normalizes fields', () => {
+    const normalizedFields = normalizeFields(mixedFields);
+    expect(normalizedFields).toEqual({
+      ownerId: '1',
+      name: 'Spending Group',
+      spending: [
+        'Mixed Spending',
+        { spendingGroupId: '2', amount: [] },
+        {
+          spendingGroupId: '2',
+          amount: [{ spendingOne: 100, spendingTwo: 200 }],
+        },
+      ],
+      spenders: [[{ spenderName: 'Talel Dayekh' }], []],
+    });
+  });
 });
--- server/src/api/utils/normalizeFields.ts
+++ server/src/api/utils/normalizeFields.ts
@@ -3,71 +3,39 @@
  * field names to resolver field names.
  */
 
-export const normalizeFields = () => {}
+export const normalizeFields = (dbFields: {} | []): {} => {
+  const normalizedFields =
+    dbFields instanceof Array
+      ? Object.values(dbFields).map((dbField) => {
+          if (typeof dbField !== 'object') return dbField;
+          return normalizeFields(dbField);
+        })
+      : Object.entries(dbFields)
+          .map((dbField) => {
+            if (dbField[0].includes('_')) {
+              const snakeCaseLettersRegEx = /(?<=_).{1}/g;
+              const snakeCaseLetters = dbField[0].match(snakeCaseLettersRegEx);
+              const resolverField = dbField[0]
+                .replace(snakeCaseLettersRegEx, () =>
+                  snakeCaseLetters!.shift()!.toUpperCase(),
+                )
+                .replace(/_/g, '');
 
+              dbField[0] = resolverField;
+            }
 
+            if (dbField[1] instanceof Array || typeof dbField[1] === 'object') {
+              dbField[1] = normalizeFields(dbField[1]!);
+            }
 
+            return dbField;
+          })
+          .reduce((normalizedFields, dbFields) => {
+            return {
+              ...normalizedFields,
+              [dbFields[0]]: dbFields[1],
+            };
+          }, {});
 
-
-
-
-
-// /*
-//  * Helper function for converting database
-//  * field names to resolver field names.
-//  */
-
-// export const normalizeFields = (dbTable: {}): {} => {
-//   const normalizedFields = Object.entries(dbTable)
-//     .map((dbRow) => {
-//       if (dbRow[0].includes('_')) {
-//         const snakeCaseLettersRegEx = /(?<=_).{1}/g;
-//         const snakeCaseLetters = dbRow[0].match(snakeCaseLettersRegEx);
-//         const resolverFieldNames = dbRow[0]
-//           .replace(snakeCaseLettersRegEx, () =>
-//             snakeCaseLetters!.shift()!.toUpperCase(),
-//           )
-//           .replace(/_/g, '');
-
-//         dbRow[0] = resolverFieldNames;
-//       }
-
-
-
-//       if (dbRow[1] instanceof Array) {
-//         const normalizedFields = dbRow[1].map((field) => {
-//           if (typeof field !== 'object') {
-//             // console.log(field);
-//             return field;
-//           }
-
-//           // !!
-//           if (field instanceof Array) {
-//             const normalizedFields = field.map((bla) => {
-//               if (typeof bla !== 'object') {
-//                 return bla
-//               }
-
-//               return normalizeFields(bla)
-//             })
-//             return normalizedFields
-//           }
-//           // !!
-
-//           return normalizeFields(field);
-//         });
-//         console.log(normalizedFields)
-//         dbRow[1] = normalizedFields;
-//       }
-
-//       return dbRow;
-//     })
-//     .reduce((normalizedDBFields, dbFields) => {
-//       return {
-//         ...normalizedDBFields,
-//         [dbFields[0]]: dbFields[1],
-//       };
-//     }, {});
-
-//   return normalizedFields;
-// };
+  return normalizedFields;
+};
