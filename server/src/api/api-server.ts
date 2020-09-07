26d2540550706588674c54263b6fd7ce7e1c1ee4
taleldayekh <mail@talel.se>
2020-09-07 22:29:21 +0200

    GraphQL middleware

--- server/src/api/api-server.ts
+++ server/src/api/api-server.ts
@@ -1,10 +1,12 @@
 import { createHTTPServerStream, createRouter } from '../shared/web-server';
 import { graphQLMiddleware } from './middleware/graphql';
+import schema from './schema';
+import { root } from './root';
 import config from '../config';
 
 const port = config.serverPort;
 const server$ = createHTTPServerStream(port);
 const router = createRouter();
 
-router.addRoute('/api', graphQLMiddleware);
+router.addRoute('/api', graphQLMiddleware(schema, root));
 server$.pipe(router.routes()).subscribe();
