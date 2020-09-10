934512121dc68e68f5a1cf935c77e9fcbc46c8d7
taleldayekh <mail@talel.se>
2020-09-10 19:49:35 +0200

    Unsubscribe to server connection after each test

--- server/src/shared/tests/integration/web-server.spec.ts
+++ server/src/shared/tests/integration/web-server.spec.ts
@@ -1,19 +1,24 @@
 import axios from 'axios';
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { createHTTPServerStream } from '../../web-server';
 import { HTTP } from '../../../interfaces/web-server.interface';
 
 describe('web server', () => {
+  let serverSubscription: Subscription;
+
   const helloWorldMiddleware = (http: HTTP): void => {
     const { res } = http;
     res.end('Hello World');
   };
 
-  afterEach(() => {});
+  afterEach(() => {
+    serverSubscription.unsubscribe();
+  });
 
   test('server stream works with basic middleware', async () => {
     const server$ = createHTTPServerStream(5000);
-    server$.pipe(map(helloWorldMiddleware)).subscribe();
+    serverSubscription = server$.pipe(map(helloWorldMiddleware)).subscribe();
 
     const res = await axios.get('http://localhost:5000');
     expect(res.data).toEqual('Hello World');
