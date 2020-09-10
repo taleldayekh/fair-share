d0f8724c985a28ca5b7b6a92168ca00fe248b37f
taleldayekh <mail@talel.se>
2020-09-10 22:59:09 +0200

    Removing repetitive code

--- server/src/shared/tests/integration/web-server.spec.ts
+++ server/src/shared/tests/integration/web-server.spec.ts
@@ -1,52 +1,53 @@
 import axios from 'axios';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { createHTTPServerStream, createRouter } from '../../web-server';
-import { HTTP } from '../../../interfaces/web-server.interface';
+import { HTTP, Router } from '../../../interfaces/web-server.interface';
 
 describe('web server', () => {
   const PORT = 5000;
+  const URL = `http://localhost:${PORT}`;
+  let server$: Observable<HTTP>;
   let serverSubscription: Subscription;
+  let router: Router;
 
   const helloWorldMiddleware = (http: HTTP): void => {
     const { res } = http;
     res.end('Hello World');
   };
 
+  beforeEach(() => {
+    server$ = createHTTPServerStream(PORT);
+    router = createRouter();
+  });
+
   afterEach(() => {
     serverSubscription.unsubscribe();
   });
 
   test('server stream works with middleware', async () => {
-    const server$ = createHTTPServerStream(PORT);
     serverSubscription = server$.pipe(map(helloWorldMiddleware)).subscribe();
+    const res = await axios.get(URL);
 
-    const res = await axios.get(`http://localhost:${PORT}`);
     expect(res.data).toEqual('Hello World');
     expect(res.status).toBe(200);
   });
 
   test('routing works with middleware', async () => {
-    const server$ = createHTTPServerStream(PORT);
-    const router = createRouter();
-
     router.addRoute('GET', '/hello-world', helloWorldMiddleware);
     serverSubscription = server$.pipe(router.routes()).subscribe();
+    const res = await axios.get(`${URL}/hello-world`);
 
-    const res = await axios.get(`http://localhost:${PORT}/hello-world`);
     expect(res.data).toEqual('Hello World');
     expect(res.status).toBe(200);
   });
 
   test('returns 405 if post request is made to get request route', async () => {
-    const server$ = createHTTPServerStream(PORT);
-    const router = createRouter();
-
     router.addRoute('GET', '/hello-world', helloWorldMiddleware);
     serverSubscription = server$.pipe(router.routes()).subscribe();
 
     try {
-      await axios.post(`http://localhost:${PORT}/hello-world`);
+      await axios.post(`${URL}/hello-world`);
     } catch (err) {
       expect(err.response.data).toEqual('405 Method Not Allowed');
       expect(err.response.status).toBe(405);
@@ -54,14 +55,11 @@ describe('web server', () => {
   });
 
   test('returns 405 if get request is made to post request route', async () => {
-    const server$ = createHTTPServerStream(PORT);
-    const router = createRouter();
-
     router.addRoute('POST', '/hello-world', helloWorldMiddleware);
     serverSubscription = server$.pipe(router.routes()).subscribe();
 
     try {
-      await axios.get(`http://localhost:${PORT}/hello-world`);
+      await axios.get(`${URL}/hello-world`);
     } catch (err) {
       expect(err.response.data).toEqual('405 Method Not Allowed');
       expect(err.response.status).toBe(405);
@@ -69,13 +67,10 @@ describe('web server', () => {
   });
 
   test('returns 404 if request is made to non-added route', async () => {
-    const server$ = createHTTPServerStream(PORT);
-    const router = createRouter();
-
     serverSubscription = server$.pipe(router.routes()).subscribe();
 
     try {
-      await axios.get(`http://localhost:${PORT}`);
+      await axios.get(URL);
     } catch (err) {
       expect(err.response.data).toBe('404 Not Found');
       expect(err.response.status).toBe(404);
