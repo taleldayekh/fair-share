f1244e064ba93d3634620d034f02b2823629ca7e
taleldayekh <mail@talel.se>
2020-09-23 18:11:34 +0200

    Removing commented out code

--- server/src/api/tests/integration/spending-group/mutations/root.spec.ts
+++ server/src/api/tests/integration/spending-group/mutations/root.spec.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import { graphQLRequestConfig } from '../../../utils';
-// import { SPENDING_GROUPS } from '../../../constants';
-// import { TALEL_ID } from '../../../../../shared/testing/test-data/constants';
 
 describe('spending group root mutations', () => {
   test('createSpendingGroup', async () => {
-    // const userId = TALEL_ID;
-    // const spendingGroup = SPENDING_GROUPS.retreat;
 
     const createSpendingGroupMutation = /* GraphQL */ `
       mutation createSpendingGroup($userId: String!, $name: String!) {
