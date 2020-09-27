7767c764d113ef3b604b673e0c439750e27804f2
taleldayekh <mail@talel.se>
2020-09-27 18:58:42 +0200

    Clean slate

--- server/src/api/tests/integration/spending/mutations/root.spec.ts
+++ server/src/api/tests/integration/spending/mutations/root.spec.ts
@@ -1,40 +1,9 @@
-// import axios from 'axios';
-// import { GRAPHQL_ENDPOINT } from '../../../constants';
-// import {
-//   SPENDING_GROUP_GROCERIES_ID,
-//   TALEL_ID,
-// } from '../../../../../shared/testing/test-data/constants';
+import axios from 'axios';
+import {
+  SPENDING_GROUP_GROCERIES_ID,
+  TALEL_ID,
+} from '../../../../../shared/testing/test-data/constants';
 
-// describe('spending root mutation', () => {
-//   test('new spending', async () => {
-//     const userId = TALEL_ID;
-//     const spendingGroupId = SPENDING_GROUP_GROCERIES_ID;
-//     const label = 'Lidl';
-//     const amount = 39;
-
-//     const newSpendingMutation = /* GraphQL */ `
-//       mutation {
-//         newSpending(userId: ${userId}, spendingGroupId: ${spendingGroupId}, label: ${label}, amount: ${amount}) {
-//           userId
-//           spendingGroupId
-//           label
-//           amount
-//         }
-//       }
-//     `;
-
-//     const res = await axios({
-//       url: GRAPHQL_ENDPOINT,
-//       method: 'post',
-//       data: {
-//         query: JSON.stringify(newSpendingMutation),
-//       },
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json',
-//       },
-//     });
-
-//     console.log(res.data);
-//   });
-// });
+describe('spending root mutations', () => {
+  test('new spending', async () => {});
+});
