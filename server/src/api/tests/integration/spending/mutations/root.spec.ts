a468f4324863bcd797f7c599711a60370b84a4a8
taleldayekh <mail@talel.se>
2020-09-29 19:50:54 +0200

    Spending tests uses axios

--- server/src/api/tests/integration/spending/mutations/root.spec.ts
+++ server/src/api/tests/integration/spending/mutations/root.spec.ts
@@ -1,9 +1,49 @@
 import axios from 'axios';
-import {
-  SPENDING_GROUP_GROCERIES_ID,
-  TALEL_ID,
-} from '../../../../../shared/testing/test-data/constants';
+import { graphQLRequestConfig } from '../../../utils';
+
+// TODO: Pass arguments to the GraphQL request instead of using hardcoded values
+// https://graphql.org/graphql-js/passing-arguments/
 
 describe('spending root mutations', () => {
-  test('new spending', async () => {});
+  test('can add new spending with integer amount', async () => {
+    const newSpendingMutation = /* GraphQL */ `
+      mutation {
+        newSpending(
+          userId: "1"
+          spendingGroupId: "1"
+          label: "Lidl"
+          amount: 66
+        ) {
+          label
+          amount
+        }
+      }
+    `;
+
+    const res = await axios(graphQLRequestConfig(newSpendingMutation));
+    expect(res.data).toMatchObject({
+      data: { newSpending: { label: 'Lidl', amount: 66 } },
+    });
+  });
+
+  test('can add new spending with float amount', async () => {
+    const newSpendingMutation = /* GraphQL */ `
+      mutation {
+        newSpending(
+          userId: "1"
+          spendingGroupId: "1"
+          label: "Rewe"
+          amount: 19.86
+        ) {
+          label
+          amount
+        }
+      }
+    `;
+
+    const res = await axios(graphQLRequestConfig(newSpendingMutation));
+    expect(res.data).toMatchObject({
+      data: { newSpending: { label: 'Rewe', amount: 19.86 } },
+    });
+  });
 });
