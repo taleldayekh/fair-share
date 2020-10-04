8ba950c657157fa55120c375217ea3f9ab63f809
taleldayekh <mail@talel.se>
2020-10-04 15:07:34 +0200

    Deleting custom request class in favor of axios

--- server/src/api/tests/utils.ts
+++ server/src/api/tests/utils.ts
@@ -1,59 +1,16 @@
 import { AxiosRequestConfig } from 'axios';
-import { GRAPHQL_ENDPOINT } from './constants';
 
-const graphQLRequestConfig = (graphQLRequest: string): AxiosRequestConfig => {
+// TODO: Load PORT number from env variable
+
+export const graphQLRequestConfig = (
+  graphQLRequest: string,
+): AxiosRequestConfig => {
   return {
+    url: 'http://localhost:666/api',
     method: 'POST',
-    url: GRAPHQL_ENDPOINT,
     headers: {
       'Content-Type': 'application/json',
     },
-    data: {query: graphQLRequest, variables: {userId: '1', name: 'Retreat'}},
-    
+    data: graphQLRequest,
   };
 };
-
-import http, { ServerResponse, IncomingMessage } from 'http';
-
-const options = {
-  port: 666,
-  host: 'localhost',
-  path: '/api',
-  method: 'POST',
-};
-
-
-
-
-
-class GraphQLRequest {
-  private _request(reqData: string): Promise<ServerResponse> {
-    return new Promise((resolve, reject) => {
-      const req = http.request(options, (res: IncomingMessage) => {
-        let resBody: string;
-        res.on('data', (chunk) => {
-          resBody = chunk.toString();
-        });
-        res.on('end', () => {
-          resolve(JSON.parse(resBody));
-        });
-      });
-
-      req.on('error', (err) => {
-        reject(err);
-      });
-      req.write(reqData);
-      req.end();
-    });
-  }
-
-  public async query(reqData: string) {
-    return await this._request(reqData);
-  }
-
-  public async mutation(reqData: string) {
-    return await this._request(reqData);
-  }
-}
-
-export { GraphQLRequest, graphQLRequestConfig };
