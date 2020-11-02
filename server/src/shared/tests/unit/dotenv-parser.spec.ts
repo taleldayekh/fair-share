87418963a716fe6444dbab2b05d9fa241bdacee4
taleldayekh <mail@talel.se>
2020-11-02 08:30:04 +0100

    Adding and removing dotenv in setup and teardown

--- server/src/shared/tests/unit/dotenv-parser.spec.ts
+++ server/src/shared/tests/unit/dotenv-parser.spec.ts
@@ -1,11 +1,15 @@
 import { SERVER_BASE_DIR } from '../../testing/test-data/constants';
-import { writeFileSync } from 'fs';
+import { unlinkSync, writeFileSync } from 'fs';
+
+const testDotenvFile = '.env.testing';
 
 beforeAll(() => {
-  const testDotenvFile = '.env.testing';
   const testDotenvData = 'DEVELOPER_NAME=TALEL\nDEVELOPER_AGE=34';
-
   writeFileSync(`${SERVER_BASE_DIR}/${testDotenvFile}`, testDotenvData);
+});
+
+afterAll(() => {
+  unlinkSync(`${SERVER_BASE_DIR}/${testDotenvFile}`);
 });
 
 describe('dotenv-parser', () => {
